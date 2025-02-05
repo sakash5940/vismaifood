b84aa61dad8e2e6b0fa68c0c860e93f2b4d1a193
sakash5940 <akashpatil5940@gmail.com>
2025-02-05 11:21:59 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,41 @@
+// components
+// import "./app.css"
+// function App(){
+//    const name="akash patil!"
+//  const age=24
+//    const ages=["24","43","27"]
+//    const names=["sumanth reddy","akash","vamshi"]
+//     console.log("hello how are you!");
+//   return(
+//      <div>
+//       <h1>my name is {name}</h1>
+//        <h3>age {age}</h3>
+//        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ratione fugit voluptate esse. Dolorem doloremque ratione laborum obcaecati sint, ducimus quos dolor, rerum maiores voluptatibus laudantium! Accusantium modi veniam quis.</p>
+//       <img src="https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg" alt="" />
+//       <br />
+//       <a href="https://media.istockphoto.com/id/1403500817/photo/the-craggies-in-the-blue-ridge-mountains.jpg?s=612x612&w=0&k=20&c=N-pGA8OClRVDzRfj_9AqANnOaDS3devZWwrQNwZuDSk=">img </a>
+//       </div>/
+//     <div>
+//      </div>
+//         )
+//        }
+//  export default App;
+import Header from "./Componants/Header/Header"
+ import Banner from "./Componants/Banner/Banner"
+ import Cards from "./Componants/Cards/Cards"
+ import Footer from "./Componants/Footer/Footer"
+
+const App=()=>{
+  return(
+  <>
+  <Header></Header>
+   <Banner></Banner>
+   <Cards></Cards>
+   <Footer></Footer>   
+  </>
+  )
+}
+export default App
+  
+  
+  
